4baf2d40cf103eadebd54dac66fc4c939b9e365d
Gene Hall <genehall@eworldes.com>
2023-05-06 11:23:13 -1000

    update breedlist on animal change

--- src/search.tsx
+++ src/search.tsx
@@ -1,7 +1,27 @@
-type Animal = "dog" | "cat" | "bird";
+import { useEffect, useState } from "react";
+const ANIMALS: Animal[] = ["dog", "cat", "bird", "rabbit", "reptile"];
+
+type Animal = "dog" | "cat" | "bird" | "rabbit" | "reptile";
 
 const Search = () => {
-  const animals: Animal[] = ["dog", "cat", "bird"];
+  const [animal, setAnimal] = useState("" as Animal);
+  const [breeds, setBreeds] = useState<string[]>([]);
+
+  useEffect(() => {
+    fetchBreedList();
+  }, [animal]);
+
+  const fetchBreedList = async () => {
+    if (!animal) {
+      return;
+    }
+
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+    );
+    const json = await res.json();
+    setBreeds(json.breeds ?? []);
+  };
 
   return (
     <div>
@@ -12,16 +32,29 @@ const Search = () => {
         </label>
         <label htmlFor="animal">
           Animal:
-          <select name="animal">
-            <option value="" />
-            {animals.map((a) => (
-              <option>{a}</option>
+          <select
+            name="animal"
+            value={animal}
+            onChange={(e) => setAnimal(e.target.value as Animal)}
+          >
+            <option value={""} />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
             ))}
           </select>
         </label>
         <label htmlFor="breed">
           Breed:
-          <input type="select" name="breed" />
+          <select name="breed">
+            <option value={""} />
+            {breeds.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
         </label>
       </form>
     </div>
