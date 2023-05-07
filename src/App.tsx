4abf8fa96a0f75c93af9034bcaffd0ee868459fe
Gene Hall <genehall@eworldes.com>
2023-05-06 16:58:00 -1000

    add description portion of details page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Search from "./search";
 import Details from "./Details";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Routes>
-          <Route path="/" element={<Search />} />
-          <Route path="/details/:id" element={<Details />} />
-        </Routes>
-      </div>
+      <QueryClientProvider client={queryClient}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/" element={<Search />} />
+            <Route path="/details/:id" element={<Details />} />
+          </Routes>
+        </div>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
--- src/Details.tsx
+++ src/Details.tsx
@@ -1,9 +1,43 @@
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import { PetAPIResponse } from "./APIResponseTypes";
+import fetchPet from "./hooks/fetchPet";
 
 const Details = () => {
   const { id } = useParams();
 
-  return <div>details page: {id} </div>;
+  if (!id) {
+    throw new Error("no id provided to detaisl");
+  }
+
+  const results = useQuery(["details", id], fetchPet);
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+
+  const pet = results?.data?.pets[0];
+
+  if (!pet) {
+    throw new Error("pet not found");
+  }
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.breed} - {pet.city}, {pet.state}
+        </h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Details;
